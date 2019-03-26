62d8580dd6cd316ff2e9f93c7229899414e2a1eb
waynewu6250 <waynewu6250@gmail.com>
2019-03-26 16:13:46 +0800

    Initial commit

---
+++ beauty_folder/beauty/items.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class BeautyItem(scrapy.Item):
+    # define the fields for your item here like:
+    title = scrapy.Field()
+    author = scrapy.Field()
+    date = scrapy.Field()
+    score = scrapy.Field()
+    pushes = scrapy.Field()
+    comments = scrapy.Field()
+    url = scrapy.Field()
+    images = scrapy.Field()
+    file_urls = scrapy.Field() #Default for download images
